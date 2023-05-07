e9e2af967d38cc8e676ea53c5c712582ef618f5b
minu <wlwhsxz@gmail.com>
2023-05-07 21:20:15 +0900

    add complete & delete todo functions

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import ListView from "./components/ListView";
 
 function App() {
   const [todoList, setTodoList] = useState([]);
+
   const handleInsert = (value) => {
     setTodoList((current) => {
       const newTodoList = [...current];
@@ -15,9 +16,30 @@ function App() {
       return newTodoList;
     });
   };
+
+  const handleComplete = (index) => {
+    setTodoList((current) => {
+      const newTodoList = [...current];
+      newTodoList[index].isCompleted = !newTodoList[index].isCompleted;
+      return newTodoList;
+    });
+  };
+
+  const handleRemove = (index) => {
+    setTodoList((current) => {
+      const newTodoList = [...current];
+      newTodoList.splice(index, 1);
+      return newTodoList;
+    });
+  };
+
   return (
     <div className="App">
-      <ListView todoList={todoList} />
+      <ListView
+        todoList={todoList}
+        onComplete={handleComplete}
+        onRemove={handleRemove}
+      />
       <InsertForm onInsert={handleInsert} />
     </div>
   );
--- src/components/ListView.js
+++ src/components/ListView.js
@@ -1,16 +1,30 @@
 import React from "react";
 
-export default function ListView({ todoList }) {
+export default function ListView({ todoList, onComplete, onRemove }) {
   return (
     <div>
       <ol>
         {todoList &&
-          todoList.map((todo) => {
+          todoList.map((todo, index) => {
             return (
               <li key={todo.key}>
                 <span>{todo.value}</span>
-                <button type="button">완료</button>
-                <button type="button">삭제</button>
+                <button
+                  type="button"
+                  onClick={() => {
+                    if (typeof onComplete === "function") onComplete(index);
+                  }}
+                >
+                  완료
+                </button>
+                <button
+                  type="button"
+                  onClick={() => {
+                    if (typeof onComplete === "function") onRemove(index);
+                  }}
+                >
+                  삭제
+                </button>
               </li>
             );
           })}
